9969ef90b375f1196a8c6065849ce190f2625729
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-03-29 04:29:17 +0700

    connect with backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,59 +1,97 @@
-import React, { useState } from 'react'
-import googleIcon from '../assets/images/icon-google.png'
-import {Link, useHistory} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import googleIcon from '../assets/images/icon-google.png';
+import { Link, useHistory } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../assets/css/LoginRegister.css'
-import FormFloating from '../component/FormFloating'
+import '../assets/css/LoginRegister.css';
+import FormFloating from '../component/FormFloating';
+import Axios from 'axios';
+var md5 = require('md5');
 
 function Login() {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [userList, setuserList] = useState();
 
-    
     console.log(username);
     console.log(password);
-    
-    const changeHandler = (e)=>{
-        setUsername(e.target.value)
-    }
-    const changeHandler2 = (e)=>{
-        setPassword(e.target.value)
-    }
-    
+
+    useEffect(() => {
+        Axios.get('http://localhost:8000/data/users').then((res) =>
+            setuserList(res.data.result)
+        );
+    }, []);
+
+    console.log(userList);
+    const changeHandler = (e) => {
+        setUsername(e.target.value);
+    };
+    const changeHandler2 = (e) => {
+        setPassword(e.target.value);
+    };
+
     let history = useHistory();
 
     const submitHandler = (e) => {
         e.preventDefault();
-        if(username==='ahikmah' && password==='12345'){
-            history.push('/dashboard/all-schedule')
-        }else{
-            alert('Wrong Username or Password!')
+        const index = userList.findIndex((x) => x.username === username);
+        console.log('index', index);
+        if (index !== -1) {
+            if (
+                username === userList[index].username &&
+                md5(password) === userList[index].password
+            ) {
+                history.push('/dashboard/all-schedule');
+            } else {
+                alert('Wrong Username or Password!');
+            }
+        } else {
+            alert('Wrong Username or Password!');
         }
-    }
-
+    };
 
     return (
         <>
-            <main className="container login">
-                <h1 className="title login">Login</h1>
-                <form className="form login" onSubmit={submitHandler}>
+            <main className='container login'>
+                <h1 className='title login'>Login</h1>
+                <form className='form login' onSubmit={submitHandler}>
+                    <FormFloating
+                        type='text'
+                        id='username'
+                        ph='email@example.com'
+                        label='Username or Email'
+                        changed={changeHandler}
+                    />
+                    <FormFloating
+                        type='password'
+                        id='password'
+                        ph='Password'
+                        label='Password'
+                        changed={changeHandler2}
+                    />
 
-                    <FormFloating type= "text" id= "username" ph= "email@example.com" label= "Username or Email" changed={changeHandler}/>
-                    <FormFloating type= "password" id= "password" ph= "Password" label = "Password" changed={changeHandler2}/>
+                    <Link
+                        to='/reset-password'
+                        className='d-flex justify-content-end forgot-password'
+                    >
+                        Forgot password?
+                    </Link>
 
-                    <Link to='/reset-password' className="d-flex justify-content-end forgot-password">Forgot password?</Link>
-
-                    <div className="button-group d-flex flex-column">
-                        <button type="submit" className="btn login btn-primer">Login</button>
-                        <button type="submit" className="btn login btn-google">
-                            <img src={googleIcon} alt="google-icon"/> Login with Google</button>
+                    <div className='button-group d-flex flex-column'>
+                        <button type='submit' className='btn login btn-primer'>
+                            Login
+                        </button>
+                        <button type='submit' className='btn login btn-google'>
+                            <img src={googleIcon} alt='google-icon' /> Login
+                            with Google
+                        </button>
                     </div>
                 </form>
-                <p className="txtlogin">New user? <Link to="/register"> Register</Link></p>
-
+                <p className='txtlogin'>
+                    New user? <Link to='/register'> Register</Link>
+                </p>
             </main>
         </>
-    )
+    );
 }
 
-export default Login
+export default Login;
